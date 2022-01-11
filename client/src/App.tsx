175e698c87dfbee937ae60e2d3bddb6a8c417849
Color-Kat <katik7979@gmail.com>
2022-01-11 15:14:20 +0300

    vite react project init, tailwind, smart_contracts_folder

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
